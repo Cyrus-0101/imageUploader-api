66492140036d80b4e32b987c1458481d29390b8b
Toxic254 <cyrus_g@outlook.com>
2021-12-05 16:52:53 +0300

    Added helmet for API protect0ion, trimmed out unnecessary .code.

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
+const dotenv = require('dotenv').config();
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const helmet = require('helmet');
+
 
 require("./mvc/models/db");
 
-const indexRouter = require('./mvc/routes/index');
 const usersRouter = require('./mvc/routes/users');
 
 const app = express();
+app.use(helmet())
 
 app.set('views', path.join(__dirname, 'mvc', 'views'));
 app.set('view engine', 'ejs');
@@ -17,19 +20,9 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', (req, res, next) => {
-	res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-	next();
-});
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-
+app.use('/', usersRouter);
 
 app.use(function(req, res, next) {
 	next(createError(404));
