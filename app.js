e1758e9c9c6b5a5fc2523fc34b4ff717b7bdfda5
Cyrus-0101 <cyrus_g@outlook.com>
2024-09-30 20:16:45 +0300

    chore: Refactor dotenv function

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const dotenv = require('dotenv').config();
+require('dotenv').config();
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
